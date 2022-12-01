c2cbc915094351f996e69b99751edd22a89640ea
TheKingOfCringe11 <95687516+TheKingOfCringe11@users.noreply.github.com>
2022-12-01 20:05:24 +0000

    Added scissor

--- SlowAndReverb/Game/Window.cs
+++ SlowAndReverb/Game/Window.cs
@@ -98,6 +98,8 @@ namespace SlowAndReverb
         private string a = "";
         private void OnUpdate(FrameEventArgs args)
         {
+            _needler.Texture.SaveAsPng("n.png");
+
             _drunkMaterial.Time += 0.1f;
 
             float amogus = 0.2f;
