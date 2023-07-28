27a4978cf31242429957d5b68473b3bba7448a4c
TheKingOfCringe11 <95687516+TheKingOfCringe11@users.noreply.github.com>
2023-07-29 02:02:14 +0300

    adad

--- SlowAndReverb/Engine/Rendering/OpenGL/UniformBlockItem.cs
+++ SlowAndReverb/Engine/Rendering/OpenGL/UniformBlockItem.cs
@@ -8,43 +8,6 @@ namespace SlowAndReverb
 {
     public struct UniformBlockItem
     {
-        private const int ScalarSize = 4;
-        private const int Vector4Size = ScalarSize * 4;
 
-        private UniformBlockItem(int size)
-        {
-            BaseAlignment = size;
-        }
-
-        public int BaseAlignment { get; private init; }
-
-        public static UniformBlockItem Scalar()
-        {
-            return new UniformBlockItem(ScalarSize);
-        }
-
-        public static UniformBlockItem Vector2()
-        {
-            return new UniformBlockItem(ScalarSize * 2);
-        }
-
-        {
-            return new UniformBlockItem(ScalarSize * 4);
-        }
-
-        public static UniformBlockItem Vector4()
-        {
-            return new UniformBlockItem(Vector4Size);
-        }
-
-        public static UniformBlockItem Array(int length)
-        {
-            return new UniformBlockItem(Vector4Size * length);
-        }
-
-        public static UniformBlockItem Matrix(int culomns)
-        {
-            return new UniformBlockItem(Vector4Size * culomns);
-        }
     }
 }
